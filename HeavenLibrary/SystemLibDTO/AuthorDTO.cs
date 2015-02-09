16bf2992382e09e3d8b5a8e9c4dfac9a6775ad85
laje1333 <jesper-larsson@live.com>
2015-02-09 14:02:18 +0100

    nya

---
+++ HeavenLibrary/SystemLibDTO/AuthorDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemLibDTO
+{
+    public class AuthorDTO
+    {
+        public int _aId;
+        public string _firstName, _lastName, _birthYear;
+        
+    }
+}
